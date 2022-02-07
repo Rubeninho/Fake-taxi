e992910f83d584a77fee968eaea5244c4447c5f4
FilipEdlund01 <filip.edlund@student.gyarab.cz>
2022-02-07 21:27:58 +0100

    longitude/latitude => to php fix

--- script.js
+++ script.js
@@ -20,6 +20,11 @@ tableCreate.appendChild(createRide);
 function showLocation(position) {
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
+
+    /*posialni dat na server pomoci php */
+    $.post("send_location.php", latitude);
+    $.post("send_location.php", longitude);
+    
     alert("Latitude : " + latitude + " Longitude: " + longitude);
  }
 
