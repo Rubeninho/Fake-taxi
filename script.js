498ad123e75e6ef442d961f10efd3786bb63a5d3
Rubeninho <90384253+Rubeninho@users.noreply.github.com>
2022-02-07 20:57:45 +0100

    KAHOUNOVI PALCE LÍŽU RAD

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-const jsonData = require('./data.json')
-jsonData
+var document = main.html;
+let tableCreate = document.getElementById("picavelka");
+let tables = [];
 
-const test = document.getElementById("testlmao");
+for (let i = 0; i < 10; i++)    {
+    tables.push(i);
+}
+let tableHelp = ["username", "email", "phone", "place", "time"]
+tables = tables.concat(tableHelp);
 
-test.value += "New Text";
+for(let tableID in tables)  {
+    let tableText = tables[tableID];
+let createRide = document.createElement("table");
+table.innerHTML = tableText;
+table.setAttribute("id", "abc" + tableID);
+table.setAttribute("class", "tableStyle");
+tableCreate.appendChild(createRide);
+}
+
+function showLocation(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    alert("Latitude : " + latitude + " Longitude: " + longitude);
+ }
+
+ function errorHandler(err) {
+    if(err.code == 1) {
+       alert("Error: Access is denied!");
+    } else if( err.code == 2) {
+       alert("Error: Position is unavailable!");
+    }
+ }
+    
+ function getLocation() {
+
+    if(navigator.geolocation) {
+       var options = {timeout:60000};
+       navigator.geolocation.getCurrentPosition(showLocation, errorHandler, options);
+    } else {
+       alert("Sorry, browser does not support geolocation!");
+    }
+ }
