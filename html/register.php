695173c09bac16bb3134d5468f7cec6457e5d49a
FilipEdlund01 <filip.edlund@student.gyarab.cz>
2022-03-01 16:15:53 +0100

    cleaning files

--- html/register.php
+++ html/register.php
@@ -17,25 +17,19 @@ if ($_SERVER["REQUEST_METHOD"] == "post") {
     // melo by se to spustit po stisknuti tlacitka submit
     // a colectnout data
 
-		if (empty($password)||empty($password_repeated)) {
-      echo "Fill the line with your password please";
-
-  }else if($password == $password_repeated){
+		
+  
 		// colletcting form data in variables
 
 		//then we must insert them in the table
 		$qry = "INSERT INTO `users` (`username`, `email`, `phone`, `password`) VALUES ('$username', '$email', $phone, '$password')";
 		// phone neni v uvozovkach protoze je to integer
 		$insert = mysqli_query($con, $qry);
-		if(!$insert){
-			echo "There are some problems while conecting to database ";
-		}else{
+		if(	echo "There are some problems while conecting to database ";
+		}else{!$insert){
+		
 		echo "Data Inserted";
 		}
-  }else{
-
-    echo"Please the passwords are not same check them again."
-
-  }
+  
 }
 ?>
