e785250fe6b305814086e34f8ccaebe2ba336a26
DanielHort <daniel.hort@student.gyarab.cz>
2022-03-01 16:23:10 +0100

    fixed registration

--- html/register.php
+++ html/register.php
@@ -7,13 +7,11 @@ $con = new mysqli($host,$user,$pass,$db);
 if(!$con){
 	echo"There are some problems conecting the database";
 }
-$name  = $_POST['username'];
+$username  = $_POST['username'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 $password = $_POST['password'];
-$password_repeated = $_POST['password_repeated'];
 
-if ($_SERVER["REQUEST_METHOD"] == "post") {
     // melo by se to spustit po stisknuti tlacitka submit
     // a colectnout data
 
@@ -25,11 +23,9 @@ if ($_SERVER["REQUEST_METHOD"] == "post") {
 		$qry = "INSERT INTO `users` (`username`, `email`, `phone`, `password`) VALUES ('$username', '$email', $phone, '$password')";
 		// phone neni v uvozovkach protoze je to integer
 		$insert = mysqli_query($con, $qry);
-		if(	echo "There are some problems while conecting to database ";
-		}else{!$insert){
-		
-		echo "Data Inserted";
-		}
-  
-}
+    if(!$insert){
+        echo "There are some problems while conecting to database ";
+    }else{
+    echo "Data Inserted";
+    }
 ?>
